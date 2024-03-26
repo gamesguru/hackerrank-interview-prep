a17f9120a7503c35e9595202d8106827d6d1764e
Shane Jaroch <chown_tee@proton.me>
2024-03-26 00:37:31 -0400

    wip challenges -> dynamic-array

---
+++ challenges/dynamic-array.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dynamicArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+
+def dynamicArray(n, queries):
+    return []
+
+
+if __name__ == "__main__":
+    n = 2
+    q = 5
+
+    queries = [
+        [1, 0, 5],
+        [1, 1, 7],
+        [1, 0, 3],
+        [2, 1, 0],
+        [2, 1, 1],
+    ]
+
+    result = dynamicArray(n, queries)
+    print(result)
