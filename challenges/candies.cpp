9efd761ad3f0d05885aa3b8a10695d65266253e9
Shane Jaroch <chown_tee@proton.me>
2024-04-10 15:11:00 -0400

    wip

--- challenges/candies.cpp
+++ challenges/candies.cpp
@@ -25,11 +25,52 @@ long candies(int n, vector<int> arr) {
 
     long total = 0;
     int c = 1;
-    for (int i=0; i<n-1; i++) {
-        while (arr[i]
-    }
+    for (int i=0; i<n; i++) {
+        // add current index
+        total += c;
 
-    std::cout << total << "\n";
+        std::cout << "c= " << c << "\n";
+
+        // handle forward step
+        if (arr[i+1] > arr[i]) {
+            c++;
+            continue;
+        }
+
+//         // handle plateau?
+//         if (arr[i+1] == arr[i])
+//             continue;
+
+        // handle backward or plateau trend
+        for (int j=i+1; j<n-1; j++) {
+            if (arr[j+1] > arr[j])
+                break;
+            c--;
+        }
+    }
+//     for (int i=0; i<n; i++) {
+//         // handle base case
+//         total += c;
+//
+//         // handle forward trends: 1 4 5 8 10
+//         if (arr[i+1] > arr[i])
+//             c++;
+//
+//         // handle backward trends (10, 8, 5, 2, 1)
+// //         for (int j=i+1; j<n-1; j++) {
+// //             if (arr[j+1] < arr[j])
+// //                 c++;
+// //             else {
+// //                 i = j;
+// //                 break;
+// //             }
+// //         }
+//         std::cout << "i= " << i << "\n";
+//         std::cout << "c= " << c << "\n";
+//         std::cout << "total= " << total << "\n\n";
+//     }
+
+//     std::cout << total << "\n";
     return total;
 }
 
